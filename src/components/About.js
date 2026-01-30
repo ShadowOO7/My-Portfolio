c72e90fdbc0a4c4d4d3a1a00b718488fc2896e3a
Shekhar Nayak <90708000+ShadowOO7@users.noreply.github.com>
2026-01-30 20:11:57 +0530

    Revise About Me section with new interests and skills
    Updated personal introduction to include Generative AI and MLOps, and changed Python in the skills list.

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,10 @@ import './About.css';
           </div>
           <div className="about-content">
             <p>
-              Hello! I'm Shekhar Nayak, a passionate and dedicated computer science enthusiast with a keen interest in competitive programming and web development. I thrive on solving complex problems and building intuitive, efficient applications.
+              Hello! I'm Shekhar Nayak, a passionate and dedicated computer science enthusiast with a keen interest in competitive programming, Generative AI, MLOps, web development. I thrive on solving complex problems and building intuitive, efficient applications.
             </p>
             <p>
-              My journey in coding began with "my first 'Hello World!' in C,". Since then, I've honed my skills in various programming languages and technologies, including C++, JavaScript, React, Node.js, MongoDB and more.
+              My journey in coding began with "my first 'Hello World!' in C,". Since then, I've honed my skills in various programming languages and technologies, including C++, Python, JavaScript, React, Node.js, MongoDB and more.
             </p>
             <p>
               I'm constantly learning and exploring new technologies to expand my knowledge base and improve my craft. When I'm not coding, I like to play Football, Badminton or go to GYM.
